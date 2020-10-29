5f4914bbeb8dfd92e17c5be1db5fcfc042b18ca9
Dakota Fritz <dakfritz01@gmail.com>
2020-10-29 08:54:47 -0400

    centered text in offerings section and started on js for collapsable section

---
+++ script.js
@@ -0,0 +1,18 @@
+// function showPizzaOptions() {
+//     var pizzaOptions = document.getElementById("pizza")
+
+//     pizzaOptions.innerhtml = 
+//     `<h6>Farm Tour and Lunch</h6>
+//         <p>Groups of 10-14 - $35/Person</p>
+//         <p>Groups of 15+ – $25/person</p>
+
+//     <h6>Farm Tour and Dinner</h6>
+//         <p>Groups of 10+ - $40/Person</p></p>
+
+//     <h6>Mushroom Hunt & Lunch or Dinner</h6>
+//         <p>Groups of 4-9 - $90/Person</p>
+//         <p>Groups of 10+ – $80/person</p>
+
+//     <p>We will accommodate any dietary needs and additional wood-fired food options are available.</p>
+//     <p>We are also available for private events, please call for further information on pricing and options.</p>`
+// }
