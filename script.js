1648ea07114d550128e82beac0326051e591a59d
Dakota Fritz <dakfritz01@gmail.com>
2020-11-04 22:53:54 -0500

    add beginning simple map

--- script.js
+++ script.js
@@ -16,3 +16,46 @@
 //     <p>We will accommodate any dietary needs and additional wood-fired food options are available.</p>
 //     <p>We are also available for private events, please call for further information on pricing and options.</p>`
 // }
+
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const ourLocation = { lat: 35.335171, lng: -82.642408 };
+    const millsRiver = { lat: 35.367535, lng: -82.524364 };
+    const transylvaniaFarmersMarket = { lat: 35.231995, lng: -82.730588 };
+    const saludaFarmersMarket = { lat: 35.235943, lng: -82.345406 };
+    const tryonFarmersMarket = { lat: 35.244359, lng: -82.250948 };
+    const sierraNevada = { lat: 35.430726, lng: -82.553794 };
+    const kenmure = { lat: 35.255085, lng: -82.444495 };
+
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 10,
+      center: millsRiver,
+    });
+    // The marker, positioned at Uluru
+    const marker1 = new google.maps.Marker({
+      position: ourLocation,
+      map: map,
+    });
+    const marker2 = new google.maps.Marker({
+        position: transylvaniaFarmersMarket,
+        map: map,
+      });
+    const marker3 = new google.maps.Marker({
+        position: saludaFarmersMarket,
+        map: map,
+    });
+    const marker4 = new google.maps.Marker({
+        position: tryonFarmersMarket,
+        map: map,
+    });
+    const marker5 = new google.maps.Marker({
+        position: sierraNevada,
+        map: map,
+      });
+    const marker6 = new google.maps.Marker({
+        position: kenmure,
+        map: map,
+      });
+  }
